6817003a147fcc9a6f7b8e3ce40f9950600dddcf
sh12321 <63566274+sh12321@users.noreply.github.com>
2021-10-04 02:51:15 +0300

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,6 @@
 
 export default function countCats(backyard) {
-  return backyard.reduce((acc, el) => el.filter(item => item === '^^').length + acc, initial=0);
+  return backyard.reduce((acc, el) => el.filter(item => item === '^^').length + acc, 0);
 }
 
 
