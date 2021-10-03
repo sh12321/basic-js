b6f421f15ccb0a8bab2aa1f7653dd635c62ccb84
sh12321 <63566274+sh12321@users.noreply.github.com>
2021-10-04 00:58:44 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,9 @@
 import { NotImplementedError } from '../extensions/index.js';
 
-/**
- * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
- * @return {String | Boolean} name of the team or false
- * in case of incorrect members
- *
- * @example
- * 
- * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
- *
- */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  const createDreamTeam = (members) => {
-    if (typeof(members) !== 'object') return false;
-    members = members.filter((el) => typeof(el) === 'string' && el.trim()[0].toUpperCase() === el.trim()[0]).map(el => el.trim()[0]).sort().join('');
-    return members;
-  }
+
+const createDreamTeam = (members) => {
+  if (typeof(members) !== 'object') return false;
+  members = members.filter((el) => typeof(el) === 'string' && el.trim()[0].toUpperCase() === el.trim()[0]).map(el => el.trim()[0]).sort().join('');
+  return members;
 }
+
