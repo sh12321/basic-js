b04a992a4b92d599a0f4035064f95ebfcd7b43b8
sh12321 <63566274+sh12321@users.noreply.github.com>
2021-10-03 23:39:31 +0300

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -16,5 +16,5 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default function countCats(/* matrix */) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  const countCats = (backyard) => backyard.filter((el) => el.includes('^^')).length;
 }
