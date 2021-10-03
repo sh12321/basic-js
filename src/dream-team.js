4c693048ce7cd11c0e73e4cfe2396b8d9d6618f2
sh12321 <63566274+sh12321@users.noreply.github.com>
2021-10-04 01:02:03 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,7 +1,7 @@
 import { NotImplementedError } from '../extensions/index.js';
 
 
-const createDreamTeam = (members) => {
+module.exports = const createDreamTeam = (members) => {
   if (typeof(members) !== 'object') return false;
   members = members.filter((el) => typeof(el) === 'string' && el.trim()[0].toUpperCase() === el.trim()[0]).map(el => el.trim()[0]).sort().join('');
   return members;
