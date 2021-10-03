4271f9ebe79c0044ef21723cb064f6b01dc3193f
sh12321 <63566274+sh12321@users.noreply.github.com>
2021-10-04 00:30:05 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -17,7 +17,7 @@ export default function createDreamTeam(/* members */) {
   throw new NotImplementedError('Not implemented');
   const createDreamTeam = (members) => {
     if (typeof(members) !== 'object') return false;
-    members = members.filter((el) => typeof(el) === 'string').map(el => el[0].toUpperCase()).sort().join('');
+    members = members.filter((el) => typeof(el) === 'string' && el.trim()[0].toUpperCase() === el.trim()[0]).map(el => el.trim()[0]).sort().join('');
     return members;
   }
 }
